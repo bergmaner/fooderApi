fd842087332ec1e6066bf077e177641e964e1dc3
bergmaner <bergkacper@gmail.com>
2021-01-28 18:05:11 +0100

    refactoring index.ts

--- index.ts
+++ index.ts
@@ -1,29 +1,17 @@
 import express from "express";
-import bodyParser from "body-parser";
-import mongoose from "mongoose";
-import path from "path";
-import {AdminRoute, VandorRoute} from "./routes";
-import { MONGO_URI } from "./config";
+import App from "./services/express";
+import dbConnection from "./services/database";
 
-const app = express();
+const startServer = async () => {
 
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({extended: true}));
-app.use("/images", express.static(path.join(__dirname, "images")))
+    const app = express();
 
-app.use("/admin", AdminRoute);
-app.use("/vandor", VandorRoute);
+    await dbConnection();
+    await App(app);
 
-mongoose.connect(MONGO_URI,{
-    useNewUrlParser:true,
-    useUnifiedTopology: true,
-    useCreateIndex: true
-}).then(result => {
-    console.log("DB CONNECTED");
-}).catch(err => {
-    console.log("error", err)
-})
+    app.listen(8000,() => {
+        console.log("listening to port 8000")
+    })
+}
 
-app.listen(8000, () => {
-    console.log("App is running on port 8000");
-})
+startServer();
